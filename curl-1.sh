cac8b5257ceb8357680ce852d8c5b447402feacc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-01-10 14:43:22 +0000

    curl-1.sh

--- curl-1.sh
+++ curl-1.sh
@@ -1,6 +1,6 @@
 # Must be subscribed for this example to work. Have to put card data
 curl -X POST --include 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/classify?locale=en_us' \
-  -H 'X-Mashape-Key: <Your Key>' \
+  -H 'X-Mashape-Key: btSAgzlS6CmshxyNyEh24vDF8sl2p1w43h9jsnCABHsQZSfxx6' \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   --data-binary '{"title":"Kroger Vitamin A & D Reduced Fat 2% Milk","upc":"","plu_code":""}'
